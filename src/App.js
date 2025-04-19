f91230221eed35393cbacbca45fe38049d9e5bc2
Michael Badejo <138082642+Badejomichael@users.noreply.github.com>
2025-04-19 02:45:15 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom" ;
+import { Home } from './pages/home';
+import { Contact } from './pages/contact';
+import { Login } from './pages/login';
+import { Provider } from "react-redux";
+import { store } from './store';
+
+function App() {
+  return (
+    <div className="App container app-container">
+      <Provider store={store}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/login" element={<Login />}/>
+            <Route path="/contact" element={<Contact />}/>
+          </Routes>
+        </Router>
+      </Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/home.js
@@ -0,0 +1,7 @@
+export const Home = () => {
+  return (
+    <div>
+      <h1>THIS IS THE HOMEPAGE</h1> 
+    </div>
+  );
+}
